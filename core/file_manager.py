c24e14d1caef15f5a54874e4e64c468fc7c53b74
MarianoGodoy <marianogodoyg@gmail.com>
2023-12-03 22:18:34 -0300

    remove print

--- core/file_manager.py
+++ core/file_manager.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 
+
 class FileManager:
     def __init__(self, folder_path):
         self.folder_path = folder_path
@@ -8,16 +9,16 @@ class FileManager:
     def crear_carpetas(self):
         carpeta_organizados = os.path.join(self.folder_path, "Organizados")
         sub_carpetas = ["Imagenes", "Videos", "Documentos", "Musica", "Otros"]
-        
+
         if not os.path.exists(carpeta_organizados):
-            os.makedirs(carpeta_organizados) 
-            
+            os.makedirs(carpeta_organizados)
+
         for sub_carpeta in sub_carpetas:
             sub_carpeta_path = os.path.join(carpeta_organizados, sub_carpeta)
             if not os.path.exists(sub_carpeta_path):
-               os.makedirs(sub_carpeta_path)
-        print("Carpetas y sub-carpetas creadas con exito") 
-           
+                os.makedirs(sub_carpeta_path)
+        print("Carpetas y sub-carpetas creadas con exito")
+
     def mover_archivos(self, carpeta_organizados):
         subcarpetas = ["Imagenes", "Videos", "Documentos", "Musica", "Otros"]
         for archivo in os.listdir(self.folder_path):
@@ -25,19 +26,17 @@ class FileManager:
             if os.path.isfile(archivo_path):
                 try:
                     tipo_archivo = self.obtener_tipo_archivo(archivo_path)
-                    print(f"Archivo: {archivo}, Tipo: {tipo_archivo}")
-
                     if tipo_archivo in subcarpetas:
-                       nombre_archivo = os.path.basename(archivo_path)
-                       destino_path = os.path.join(carpeta_organizados, tipo_archivo, nombre_archivo)
-                       print(f"Moviendo a: {destino_path}")
-                       shutil.move(archivo_path, destino_path)
+                        nombre_archivo = os.path.basename(archivo_path)
+                        destino_path = os.path.join(
+                            carpeta_organizados, tipo_archivo, nombre_archivo
+                        )
+                        shutil.move(archivo_path, destino_path)
                 except FileNotFoundError:
-                       print(f"Archivo no encontrado: {archivo}")
-    
+                    print(f"Archivo no encontrado: {archivo}")
+
     def obtener_tipo_archivo(self, archivo_path):
         try:
-        # Obtener la extensión del archivo
             extension = os.path.splitext(archivo_path)[1].lower()
 
             tipo_a_carpeta = {
@@ -52,11 +51,7 @@ class FileManager:
 
             tipo_carpeta = tipo_a_carpeta.get(extension, "Otros")
 
-            print(f"Archivo: {archivo_path}, Tipo: {tipo_carpeta}")
-        
             return tipo_carpeta
         except FileNotFoundError:
-           print(f"Archivo no encontrado: {archivo_path}")
+            print(f"Archivo no encontrado: {archivo_path}")
         return "Otros"
-    
-    
